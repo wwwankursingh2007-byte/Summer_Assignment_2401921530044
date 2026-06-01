cc308b264ab3c8ca0caf093a2dc4f1f3a064fba2
Ankur singh <www.ankursingh2007@gmail.com>
2026-06-01 11:34:52 +0530

    initial commit

---
+++ Week_1/Day_1/buysellstock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int buyprice=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(buyprice<prices[i]){
+                int profit=prices[i]-buyprice;
+                maxprofit=Math.max(maxprofit,profit);
+            }else{
+                buyprice=prices[i];
+            }
+        }
+        return maxprofit;
+        
+    }
+}
