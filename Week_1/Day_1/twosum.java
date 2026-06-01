9ffe14a722550c35c025001efbfc8dee6f868887
Ankur singh <www.ankursingh2007@gmail.com>
2026-06-01 12:41:44 +0530

    initial

---
+++ Week_1/Day_1/twosum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<=nums.length-1;i++){
+            int num=target-nums[i];
+        
+            if(map.containsKey(num)){
+            return new int[]{map.get(num),i};
+           }
+           map.put(nums[i],i);
+        }
+         
+        return new int[]{-1,-1};
+        
+    }
+}
