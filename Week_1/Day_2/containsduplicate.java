bd1f0efadfb5f18d1f4ea23acf09d768a26875aa
Ankur singh <www.ankursingh2007@gmail.com>
2026-06-02 10:57:57 +0530

    contains duplicate

---
+++ Week_1/Day_2/containsduplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<=nums.length-1;i++){
+            int val=nums[i];
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+       
+    }
+}
